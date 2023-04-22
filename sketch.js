697cd28841a26f42b477d204ab81b6fc50bbcc73
june1207 <131511418+june1207@users.noreply.github.com>
2023-04-22 16:45:37 +0900

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,29 +1,25 @@
-var i=0;
-var d=5;
-var on=false;
-var isIn=false
-;
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  rectMode(CENTER)
+  createCanvas(800, 800);
 }
 
 function draw() {
-  background(220);
-  i+=d;
-  if(i>255 || i<1) d=d;
-  if(mouseX>=width/2-50 && mouseX<=width/2+50 && mouseY>=height/2-50 && mouseY<=height/2+50) {
-    fill(0,0,255,i);
-  } else {
-    fill(255,0,0,i);
-  }
-  ellipse(width/2,height/2,100,30);
-  ellipse(width/2,height/2,30,100);
-  rect(width/2,height/2,100,100)
-  print(windowWidth,windowHeight,displayWidth,displayHeight)
+  h=hour();
+  m=minute();
+  s=second();
+  print(h,m,s)
+  background(0);
+  ellipse(400,400,405,405)
+  my_rect(400,400,200,5,s*6-90,color(255,0,0));
+  my_rect(400,400,200,10,m*6+s/10-90,color(0,255,0));
+  my_rect(400,400,100,15,h*30+m/2+s/120-90,color(0,0,255));
+  ellipse(400,400,30,30)
 }
 
-function windowResized() {
-  resizeCanvas(windowWidth,windowHeight)
+function my_rect(x,y,w,h,d,c) {
+  push();
+  fill(c)
+  translate(x,y);
+  rotate(radians(d));
+  rect(0,-h/2,w,h);
+  pop()
 }
-
